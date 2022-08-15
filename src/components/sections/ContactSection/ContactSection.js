3d20e975dca1c76ab72fb4825fc500cd541b18f1
Cristóbal Sánchez <ulkiobal@gmail.com>
2022-08-15 15:25:24 +0200

    Contact section spacing fixed

--- src/components/sections/ContactSection/ContactSection.js
+++ src/components/sections/ContactSection/ContactSection.js
@@ -114,8 +114,8 @@ function ContactSection({ state, fullpageApi }) {
       id="contact">
       <Box
         borderRadius="lg"
-        m={{ base: 5, md: 16, lg: 10 }}
-        p={{ base: 5, lg: 16 }}>
+        m={{ base: 2, md: 8, lg: 5 }}
+        p={{ base: 2, lg: 5 }}>
         <Box>
           <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
             <Heading
