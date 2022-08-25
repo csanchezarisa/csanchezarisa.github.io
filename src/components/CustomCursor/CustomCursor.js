583bdf82a6a779add7dcc9474721e519ed44024b
Cristóbal Sánchez <ulkiobal@gmail.com>
2022-08-25 20:37:44 +0200

    A custom cursor is created
    The default cursor is hidden and it's replaced by a custom one

---
+++ src/components/CustomCursor/CustomCursor.js
@@ -0,0 +1,115 @@
+import React, { useRef, MutableRefObject } from 'react';
+
+import './CustomCursor.css';
+
+
+/**
+ * DOM elements that change cursor to pointer type
+ */
+const pointerCursorElements = ['BUTTON', 'A', 'svg', 'path'];
+/**
+ * DOM elements that change cursor to text type
+ */
+const textCursorElements = ['INPUT', 'TEXTAREA']
+
+/**
+ * Moves the custom cursor to the mouse position
+ * @param {Event} e Event of the listener
+ * @param {MutableRefObject} cursor Div that contains the custom cursor
+ */
+const moveCursor = (e, cursor) => {
+  const mouseY = e.clientY - 5;
+  const mouseX = e.clientX - 24;
+   
+  if(!cursor.current) {
+    return;
+  }
+  
+  cursor.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+
+/**
+ * Changes the cursor display state
+ * @param {Event} e Event of the listener
+ * @param {boolean} isHidden Whether the custom cursor should be hidden or not 
+ * @param {MutableRefObject} cursor Div that contains the custom cursor
+ * @param {{ default: string, pointer: string, text: string }} images JSON with the cursor images references
+ */
+const changeCursorHidden = (e, isHidden, cursor, images) => {
+
+  // There are no div element selected
+  if(!cursor.current) {
+    return;
+  }
+
+  // The custom cursor should be hidden and it's not hidden already
+  if(isHidden && !cursor.current.classList.contains('hidden')) {
+    cursor.current.classList.add('hidden');
+  }
+
+  // The custom cursor should be displayed and it's hidden
+  else if(!isHidden && cursor.current.classList.contains('hidden')) {
+    cursor.current.classList.remove('hidden');
+  }
+
+  changeCursorStyle(e, cursor, images);
+};
+
+
+/**
+ * Changes the custom cursor style
+ * @param {Event} e Event of the listener
+ * @param {MutableRefObject} cursor Div that contains the custom cursor
+ * @param {{ default: string, pointer: string, text: string }} images JSON with the cursor images references
+ */
+const changeCursorStyle = (e, cursor, images) => {
+  const targetElement = e.target.nodeName;
+  const img = cursor.current.children[0];
+  
+  if(pointerCursorElements.includes(targetElement)) {
+    img.src = images.pointer;
+  }
+  else if(textCursorElements.includes(targetElement)) {
+    img.src = images.text;
+  }
+  else if(img.src !== images.default) {
+    img.src = images.default;
+  }
+};
+
+
+/**
+ * Adds event listeners to move and style the custom cursor
+ * @param {MutableRefObject} cursor Div that contains the custom cursor
+ * @param {{ default: string, pointer: string, text: string }} images JSON with the cursor images references
+ */
+const AddEventListeners = (cursor, images) => {
+  window.addEventListener('mousemove', (e) => moveCursor(e, cursor));
+  window.addEventListener('mouseover', (e) => changeCursorHidden(e, false, cursor, images));
+  window.addEventListener('mouseout', (e) => changeCursorHidden(e, true, cursor, images));
+};
+
+
+function CustomCursor({ 
+  defaultImg, 
+  pointerImg,
+  textImg
+}) {
+  const cursor = useRef();
+
+  AddEventListeners(cursor, { default: defaultImg, pointer: pointerImg, text: textImg });
+
+  return (
+    <>
+      <div 
+        ref={cursor}
+        className="cursor hidden"
+      >
+        <img src={defaultImg} className='cursor' alt='cursor' />
+      </div>
+    </>
+  )
+}
+
+export default CustomCursor;
