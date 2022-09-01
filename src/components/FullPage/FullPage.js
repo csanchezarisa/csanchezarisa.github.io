3505970d644db5d06d2d84152fa60c07688cd801
Cristóbal Sánchez <ulkiobal@gmail.com>
2022-09-01 21:00:04 +0200

    Scrolling overflow class

--- src/components/FullPage/FullPage.js
+++ src/components/FullPage/FullPage.js
@@ -48,7 +48,7 @@ function FullPage() {
         licenseKey={fullpageApiLicense}
         scrollingSpeed={300}
         sectionsColor={'#282c34,'.repeat(Sections.length + 2).split(',')}
-        normalScrollElements='.overflow-auto'
+        normalScrollElements={window.innerWidth <= 640 ? '.overflow-auto' : ''}
         afterLoad={(_origin, destination) => changeActiveSection(destination.item.id)}
 
         render={({ state, fullpageApi }) => {
